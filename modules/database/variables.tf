189967edf87690c6426972acf6292421167a878f
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-03-15 16:55:13 -0400

    Update variables.tf

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -1,23 +1,23 @@
 variable "db_name" {
-  description = "Name of the Azure DB for PostgreSQL instance"
+  description = "Name of the Azure DB for PostgreSQL"
 }
 
 variable "db_sku_name" {
-  description = "Name of the SKU to use for the Azure DB for PostgreSQL instance"
+  description = "Name of the SKU "
   default     = "B_Gen5_1"
 }
 
 variable "db_storage_mb" {
-  description = "Amount of storage to allocate for the Azure DB for PostgreSQL instance (in MB)"
+  description = "Amount of storage to allocate(in MB)"
   default     = 5120
 }
 
 variable "db_admin_username" {
-  description = "Username for the admin account of the Azure DB for PostgreSQL instance"
+  description = "Username for the admin account "
 }
 
 variable "db_admin_password" {
-  description = "Password for the admin account of the Azure DB for PostgreSQL instance"
+  description = "Password for the admin account"
 }
 
 
