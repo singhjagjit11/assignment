70b33ca7882444719efc5cb138d075d67293586f
singhjagjit11 <singhjagji87@gmail.com>
2023-03-14 22:04:34 -0400

    changes

---
+++ modules/common/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+	default = "N01574661-assignment1-RG"
+
+
+}
+
+variable "location" {
+	default = "eastus"
+}
+
+variable "tags" {
+  type = map(string)
+  description = "A mapping of tags to assign to the resource."
+  default = {
+    Project = "Automation Project – Assignment 1"
+    Name = "jagjit.singh"
+    ExpirationDate = "2023-06-30"
+    Environment = "Lab"
+  }
+}
+
+
+
