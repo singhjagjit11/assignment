9b1673728f6b2b6e9b52d573e7b627b2dd02b51e
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-03-19 20:06:24 -0400

    Update variables.tf

--- modules/datadisk/variables.tf
+++ modules/datadisk/variables.tf
@@ -1,17 +1,17 @@
 variable "vm_count" {
-  description = "The number of virtual machines to attach the data disks to"
+ type = string
 }
 
 variable "location" {
-  description = "The location of the resource group"
+ type = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group"
+ type = string
 }
-variable "subnet_id" {
-  description = "The ID of the subnet where the resources will be provisioned."
 
+variable "subnet_id" {
+ type = string
 }
 
 variable "tags" {
@@ -30,4 +30,12 @@ variable "linux_vm_ids" {
 
 variable "vmwindows_vm_id" {
   type = string
+}
+
+variable "disk_size" {
+    default = 10
+}
+
+variable "lun" {
+    default = 0
 }
