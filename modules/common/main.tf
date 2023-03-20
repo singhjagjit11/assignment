9aa04860aa2e1c4273c00fab653d6bbc3a29c898
singhjagjit11 <116024713+singhjagjit11@users.noreply.github.com>
2023-03-19 20:03:05 -0400

    Update main.tf

--- modules/common/main.tf
+++ modules/common/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_log_analytics_workspace" "la_workspace" {
   name                = "la-workspace4661"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
+  sku                 = var.sku
   
   tags = var.tags
 }
